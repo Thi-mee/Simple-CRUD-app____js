2497f60541fc4e490cb59edc547fc94e08415523
Timilehin Adenuga <timswaggs1@outlook.com>
2022-08-31 00:29:04 +0100

    Progress

---
+++ src/app.js
@@ -0,0 +1,82 @@
+// write your code here
+const { data } = require('./data.js')
+const express = require ('express');
+const port = 8080;
+const app = express();
+
+
+app.use(express.json());
+
+
+
+// GET for 'C' in CRUD
+app.get('/', function(req, res) {
+
+    res.status(200).json(data)
+});
+app.get('/:id', function(req, res) {
+
+    let id = req.params.id;
+    data.forEach(item => {
+        if(item(id) == id){
+            res.status(200).json(item)
+        }
+    });
+});
+
+
+//Receive for 'R' in CRUD
+app.post('/', (req, res) => {
+
+    let newCourse = req.body;
+    res.json({
+        newCourse,
+        success: "New course added successfully"
+    })
+});
+
+// Update for 'U' in CRUD
+app.put('/:id', (req, res) => {
+
+    let id = req.params.id
+    let status = req.body.status
+    //update course enrollment status
+    data.forEach(item => {
+        if(item(id) == id){
+            let index = database.indexOf(item)
+            item.status = status;
+            res.json({
+                item,
+                message: `status of Item with id ${index + 1} updated successfully`
+            })
+        }
+    })
+});
+
+
+// Delete for 'D' in CRUD
+app.delete('/:id', (req, res) => {
+
+    let id = req.params.id;
+    data.forEach(item => {
+        if(item.id == id){
+            let index = data.indexOf(item)
+            database.splice(index, 1)
+            res.status(200).json(`Item with id ${index + 1} deleted successfully`)
+        }
+    })
+})
+
+
+
+// default URL to API
+app.use('/', function(req, res){
+    res.statusCode = 200;
+    res.send('The API works :-)');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`CRUD app listening on port ${port}`)
+})
